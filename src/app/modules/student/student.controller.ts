934d1cf408113c251af889e45bb48daf77fcd59a
Sabithasan2098 <sabithasan2098@gmail.com>
2024-05-26 17:27:13 +0600

    refactor student controller to dont reapet code such as try catch block

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,56 +1,49 @@
 import { RequestHandler } from "express";
 import { studentServices } from "./student.service";
-import { z } from "zod";
-import studentValidationSchemaZod from "./student.validation";
 import { sendResponse } from "../../utils/sendResponse";
 import httpStatus from "http-status";
 
-// find all student data------------------------------------>
-const getAllStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const result = await studentServices.getAllStudentFromDB();
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Student faced is successfully",
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-// ------------------------------------------------//
-// find a student data----------------------------->
-const getAStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { studentId } = req.params;
-    const result = await studentServices.getAStudentFromDB(studentId);
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Student faced is successfully",
-      data: result,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
-// delete a student data----------------------------->
-const deleteAStudent: RequestHandler = async (req, res, next) => {
-  try {
-    const { studentId } = req.params;
-    const result = await studentServices.deleteAStudentFromDB(studentId);
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: "Delete student is successfully",
-      data: result,
-    });
-  } catch (err: any) {
-    next(err);
-  }
+// create a higher order function to don't repeat code such as try catch block--------------------------->
+
+const createHandler = (
+  serviceFunction: Function,
+  successMessage: string,
+): RequestHandler => {
+  return async (req, res, next) => {
+    try {
+      const result = await serviceFunction(req);
+      sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: successMessage,
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
 };
 
+// ----------------------------------------------------------------------------------------------------//
+// This are the refactor code result------------------------------------------------------------------->
+// find all student data---------------------------->
+const getAllStudent = createHandler(
+  () => studentServices.getAllStudentFromDB(),
+  "All student data fetched successful!",
+);
+// ------------------------------------------------//
+// find a student data----------------------------->
+const getAStudent = createHandler(
+  (req: any) => studentServices.getAStudentFromDB(req.params.studentId),
+  "Student fetched successfully",
+);
+// delete a student data--------------------------->
+const deleteAStudent = createHandler(
+  (req: any) => studentServices.deleteAStudentFromDB(req.params.studentId),
+  "Delete student successfully",
+);
+// ----------------------------------------------//
+// ----------------------------------------------------------------------------------------------------//
 export const studentControllers = {
   getAllStudent,
   getAStudent,
