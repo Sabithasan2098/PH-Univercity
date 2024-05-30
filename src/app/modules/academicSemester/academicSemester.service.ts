f0829c0f15e516c7161f2110f752513a4cff3de6
Sabithasan2098 <sabithasan2098@gmail.com>
2024-05-30 16:37:22 +0600

    academicSemester name & code simeller

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -4,6 +4,20 @@ import { AcademicSemesterModel } from "./academicSemester.model";
 export const createAcademicSemesterIntoDB = async (
   payLod: TAcademicSemester,
 ) => {
+  // checking this name and code ware right to proceed------------------------------------->
+  type TAcademicSemesterNameCodeMapper = {
+    [key: string]: string;
+  };
+
+  const academicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
+    Autumn: "01",
+    Fall: "02",
+    Summer: "03",
+  };
+  if (academicSemesterNameCodeMapper[payLod.name] !== payLod.code) {
+    throw new Error("Invalid AcademicSemester name or code");
+  }
+  // --------------------------------------------------------------------------------------//
   const result = await AcademicSemesterModel.create(payLod);
   return result;
 };
