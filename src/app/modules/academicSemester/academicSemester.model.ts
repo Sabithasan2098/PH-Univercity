fb552d8ad08679094b0baacdede52b7fc439598d
Sabithasan2098 <sabithasan2098@gmail.com>
2024-05-30 16:15:18 +0600

    Don't create a semester in same year with a same name

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -31,6 +31,19 @@ const academicSemesterSchema = new Schema<TAcademicSemester>(
   },
 );
 
+// checking to the collection is the same semester in one year is already create or not------------------------------>
+academicSemesterSchema.pre("save", async function (next) {
+  const isSemesterExists = await AcademicSemesterModel.findOne({
+    year: this.year,
+    name: this.name,
+  });
+  if (isSemesterExists) {
+    throw new Error("Semester is already exists");
+  }
+  next();
+});
+// ----------------------------------------------------------------------------------------------------------------->
+
 export const AcademicSemesterModel = model<TAcademicSemester>(
   "AcademicSemester",
   academicSemesterSchema,
